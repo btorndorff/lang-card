efe210ffac7577dd67b3c5ec93132b7f27dffa07
Ben <btorndorff@gmail.com>
2024-10-15 10:48:33 -0700

    remove old backend

--- lang-card-frontend/src/app/constants/LANGUAGE_LEVELS.ts
+++
@@ -1,2 +0,0 @@
-export const LanguageLevels = ["A1", "A2", "B1", "B2", "C1", "C2"];
-export type LanguageLevelsType = (typeof LanguageLevels)[number];
--- lang-card-frontend/src/app/constants/SUPPORTED_LANGUAGES.ts
+++
@@ -1,8 +0,0 @@
-export const SupportedLanguages = ["ja", "tl", "vi"] as const;
-export type SupportedLanguagesType = (typeof SupportedLanguages)[number];
-
-export const LanguageNameMap: Record<SupportedLanguagesType, string> = {
-  ja: "Japanese",
-  tl: "Tagalog",
-  vi: "Vietnamese",
-};
